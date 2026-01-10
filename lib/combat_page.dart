fa79b10b93f887563b2255a52321bcf1ffb0a85b
Jefferey Neuffer <jeffereyneuffer@gmail.com>
2026-01-10 19:19:17 +1000

    Allow renaming sessions

--- lib/combat_page.dart
+++ lib/combat_page.dart
@@ -1,17 +1,52 @@
+import 'package:combat_tracker/campaign_manager.dart';
 import 'package:combat_tracker/combat/combat_table.dart';
 import 'package:combat_tracker/datamodel/combat.pb.dart';
 import 'package:flutter/material.dart';
 
-class CombatPage extends StatelessWidget {
+class CombatPage extends StatefulWidget {
   const CombatPage({super.key, required this.combat});
 
   final Combat combat;
 
   @override
+  State<CombatPage> createState() => _CombatPageState();
+}
+
+class _CombatPageState extends State<CombatPage> {
+  late TextEditingController nameController;
+
+  @override
+  void initState() {
+    nameController = TextEditingController(text: widget.combat.name);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(combat.name)),
-      body: CombatTable(combat: combat),
+      appBar: AppBar(
+        title: TextField(
+          controller: nameController,
+          onChanged: (value) {
+            if (value.isNotEmpty) {
+              setState(() {
+                widget.combat.name = value;
+                CampaignManager.instance.saveCampaign();
+              });
+            }
+          },
+          decoration: InputDecoration(border: InputBorder.none),
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+      body: CombatTable(combat: widget.combat),
     );
   }
 }
