252568d28b6cd0dd9230b533ee5eedb01a7ccae6
Jefferey Neuffer <jeff@jefferey.dev>
2026-03-26 12:16:02 +1000

    Get accent color from the xdg portal where available

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:combat_tracker/campaign/campaign_manager.dart';
 import 'package:combat_tracker/settings/settings.dart';
 import 'package:combat_tracker/startup_page.dart';
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:window_manager/window_manager.dart';
+import 'package:xdg_desktop_portal/xdg_desktop_portal.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,6 +25,7 @@ class MainApp extends StatefulWidget {
 class _MainAppState extends State<MainApp> {
   bool settingsReady = false;
   bool get ready => settingsReady;
+  Color? overrideAcentColor;
 
   void loadSettingsService() async {
     await SettingsService.build();
@@ -31,9 +35,40 @@ class _MainAppState extends State<MainApp> {
     });
   }
 
+  void getAccentColor() async {
+    if (Platform.isLinux) {
+      try {
+        var client = XdgDesktopPortalClient();
+        var result = await client.settings.read(
+          "org.freedesktop.appearance",
+          "accent-color",
+        );
+        var vals = List.from(
+          result.asVariant().asStruct().map((x) => x.asDouble()),
+        );
+        if (vals.length == 3 && !vals.any((x) => x < 0 || x > 1)) {
+          var color = Color.fromARGB(
+            255,
+            (255 * vals[0]).floor(),
+            (255 * vals[1]).floor(),
+            (255 * vals[2]).floor(),
+          );
+          if (mounted) {
+            setState(() {
+              overrideAcentColor = color;
+            });
+          }
+        }
+      } catch (exception) {
+        overrideAcentColor = null;
+      }
+    }
+  }
+
   @override
   void initState() {
     loadSettingsService();
+    getAccentColor();
     super.initState();
   }
 
@@ -41,6 +76,16 @@ class _MainAppState extends State<MainApp> {
   Widget build(BuildContext context) {
     return DynamicColorBuilder(
       builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+        if (overrideAcentColor != null) {
+          lightDynamic = ColorScheme.fromSeed(
+            seedColor: overrideAcentColor!,
+            brightness: Brightness.light,
+          );
+          darkDynamic = ColorScheme.fromSeed(
+            seedColor: overrideAcentColor!,
+            brightness: Brightness.dark,
+          );
+        }
         return MaterialApp(
           title: "Combat Tracker",
           theme: ThemeData(colorScheme: lightDynamic, useMaterial3: true),
